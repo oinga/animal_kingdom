feb64f3720e4e935c5079c6fd87645bfec7871be
oinga <ortivprogrammer.net>
2022-06-14 13:54:33 -0400

    initial commit

---
+++ models/earth.py
@@ -0,0 +1,9 @@
+class Animal:
+  def __init__(self, classification, blooded, class_of, family):
+    self.classification= classification
+    self.blooded = blooded
+    self.class_of = class_of
+    self.family = family
+
+
+
---
+++ specimen.py
@@ -0,0 +1,9 @@
+from models.earth import Animal
+
+specimen_1 = Animal(
+                                    classification='vertibra', 
+                                    blooded='warm',
+                                    class_of='mammal',
+                                    family='bear')
+
+print(specimen_1.family)
